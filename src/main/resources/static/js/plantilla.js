edfc5a966da6a7e1b4b5a630393d78c402276ead
juandgg <juanguiza65@gmail.com>
2024-03-04 13:36:48 -0500

    fix: :bug: se corrigio un bug que no dejaba mostrar los datos en el front

--- src/main/resources/static/js/plantilla.js
+++ src/main/resources/static/js/plantilla.js
@@ -1,9 +1,9 @@
 const plantillaPlato = (nombre,descripcion,precio)=>{
     return `
     <tr>
-        <td>nombre</td>
-        <td>descripcion</td>
-        <td>precio</td>
+        <td>${nombre}</td>
+        <td>${descripcion}</td>
+        <td>${precio}</td>
         <td>
             <img src="../img/btnEditar.png">
             <img src="../img/btnEliminar.png">
